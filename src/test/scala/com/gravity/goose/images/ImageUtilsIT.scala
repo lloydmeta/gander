cc9d143bb68b1d5ad446377cf4dc729799d9aad2
Lloyd <lloydmeta@gmail.com>
2017-01-09 14:51:50 +0900

    Tests compile too

--- src/test/scala/com/gravity/goose/images/ImageUtilsIT.scala
+++ src/test/scala/com/gravity/goose/images/ImageUtilsIT.scala
@@ -1,9 +1,6 @@
 package com.gravity.goose.images
 
 import org.junit.Test
-import org.apache.http.client.HttpClient
-import com.gravity.goose.network.HtmlFetcher
-import com.gravity.goose.Configuration
 
 /**
  * Created by Jim Plush
@@ -14,12 +11,6 @@ import com.gravity.goose.Configuration
 
 class ImageUtilsIT {
 
-  @Test
-  def storeImageLocally() {
-    val httpClient: HttpClient = HtmlFetcher.getHttpClient
-    val imgSrc = "http://tctechcrunch2011.files.wordpress.com/2011/09/aaaaa.png?w=288m"
-    println(ImageUtils.storeImageToLocalFile(httpClient, "abc", imgSrc, new Configuration))
-  }
 
 
   @Test
